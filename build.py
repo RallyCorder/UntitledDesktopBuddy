3f6fb128a38f9543ffa5aae58b21f6cf5f433bf4
RallyCorder <gg64mail@protonmail>
2025-12-05 13:26:53 +0100

    miniature tarkovsky is!

--- build.py
+++ build.py
@@ -1,18 +1,18 @@
 import sys
 from PySide6 import QtCore,QtWidgets,QtGui
+from PySide6.QtGui import QPixmap
 
 class マナ(QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
-        
-        self.layout=QtWidgets.QGridLayout(self)
-        self.box=QtWidgets.QPushButton("マナ")
-        self.box.setMinimumSize(150,200)
-        self.layout.addWidget(self.box)
+        self.pixmap=QPixmap('Andrei_tarkovsky_stamp_russia_2007.jpg')
+        self.label=QtWidgets.QLabel(self)
+        self.label.setPixmap(self.pixmap)
         
 app=QtWidgets.QApplication([])
 widget=マナ()
-widget.resize(100,100)
+widget.setMaximumSize(200,281)
+widget.setMinimumSize(200,281)
 widget.show()
 sys.exit(app.exec())
